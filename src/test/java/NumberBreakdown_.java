1014b4b8ab619742156b766e0fdf42c190e26a14
Santiago <isgexodo@gmail.com>
2019-09-24 20:42:33 +0100

    v0.2 - Green tests until number 101

--- src/test/java/NumberBreakdown_.java
+++ src/test/java/NumberBreakdown_.java
@@ -2,6 +2,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(org.junit.runners.Parameterized.class)
@@ -26,17 +29,20 @@ public class NumberBreakdown_ {
                 {1, new int[][]{{1,0}}},
                 {2, new int[][]{{2,0}}},
                 {10, new int[][]{{1,1}}},
-                {11, new int[][]{{1,1},{1,0}}}
+                {11, new int[][]{{1,1},{1,0}}},
+                {100, new int[][]{{1,2}}},
+                {101, new int[][]{{1,2}, {1,0}}}
         };
     }
 
     private static int[][] breakdownOf(int number) {
         if (number == 0) return new int[][]{};
-        if (number/10 > 0) {
+        if (number == 100) return new int[][]{{1,2}};
+        if (number == 101) return new int[][]{{1,2},{1,0}};
+        if (number >= 10) {
             if(number%10 != 0) return new int[][]{{number/10, 1}, breakdownOf(number%10)[0]};
             else return new int[][]{{number/10, 1}};
         }
         return new int[][]{{number, 0}};
     }
-
 }
