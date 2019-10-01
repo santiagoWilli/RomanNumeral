3b7d29f81fd455b14fefe9885c0e36a17c1d22b9
santiagoWilli <santiago30mw@gmail.com>
2019-10-01 09:21:19 +0100

    v0.6.1 - Refactorized with Replace Temp With Query methodology. Next red test is a negative number

--- src/main/java/es/ulpgc/NumberBreakdown.java
+++ src/main/java/es/ulpgc/NumberBreakdown.java
@@ -9,21 +9,35 @@ public class NumberBreakdown {
         int[] length = new int[]{String.valueOf(number).length() - 1};
 
          return Arrays.stream(String.valueOf(number).split(""))
-                 .map(d -> new int[]{Integer.parseInt(d), length[0]--})
+                 .map(d -> tuple(d, length))
                  .filter(t -> isNotZero(t[0]))
                  .collect(Collectors.toList())
                  .toArray(new int[setArrayLength(number)][]);
+    }
+
+    private static int[] tuple(String d, int[] length) {
+        return new int[]{Integer.parseInt(d), length[0]--};
     }
 
     private static boolean isNotZero(int number) {
         return number != 0;
     }
 
+    private static int numberOfDigits(int number) {
+        return String.valueOf(number).length();
+    }
+
     private static int setArrayLength(int number) {
-        String numberString = String.valueOf(number);
-        int length = numberString.length();
-        int numberOfZeros = 0;
-        for(int i=0; i<length; i++) {if(numberString.charAt(i) == '0') numberOfZeros++;}
-        return length - numberOfZeros;
+        return numberOfDigits(number) - numberOfZeros(number);
+    }
+
+    private static int numberOfZeros(int number) {
+        return numberOfZeros(String.valueOf(number));
+    }
+
+    private static int numberOfZeros(String number) {
+        if(number.length() == 0) return 0;
+        if(Integer.parseInt(number.substring(0, 1)) == 0) return 1 + numberOfZeros(number.substring(1, number.length()));
+        return numberOfZeros(number.substring(1, number.length()));
     }
 }
